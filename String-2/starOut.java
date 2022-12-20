5e2869c2cbaafb1aebfbbe362055022b2f3abf1e
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2022-12-20 17:03:32 +0300

    Update starOut.java

--- String-2/starOut.java
+++ String-2/starOut.java
@@ -1,3 +1,14 @@
+/*
+Return a version of the given string, where for every star (*) in the string the star and the
+chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also
+yields "ad".
+
+
+starOut("ab*cd") → "ad"
+starOut("ab**cd") → "ad"
+starOut("sm*eilly") → "silly"
+*/
+
 public String starOut(String str) {
   if(!str.contains("*")){
     return str; //yıldız içermiyorsa değeri direkt döndürüyor

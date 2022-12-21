f1a7ba8af4533e1bc54a1be155bfd46b8c8670d8
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2022-12-21 14:07:58 +0300

    Update and rename equaIsNot.java to equalIsNot.java

--- String-2/equalIsNot.java
+++ String-2/equalIsNot.java
@@ -1,3 +1,12 @@
+/*
+Given a string, return true if the number of appearances of "is" anywhere in the
+string is equal to the number of appearances of "not" anywhere in the string (case sensitive).
+
+equalIsNot("This is not") → false
+equalIsNot("This is notnot") → true
+equalIsNot("noisxxnotyynotxisi") → true
+*/
+
 public boolean equalIsNot(String str) {
   int count1=0;
   int count2=0;

0a3eb5dc0ce6847fd62908ff2d2686046dc3fa2a
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2023-01-02 10:45:40 +0300

    Create withoutString.java

---
+++ String-3/withoutString.java
@@ -0,0 +1,25 @@
+/*
+Given two strings, base and remove, return a version of the base string where all
+instances of the remove string have been removed (not case sensitive). You may assume
+that the remove string is length 1 or more. Remove only non-overlapping instances,
+so with "xxx" removing "xx" leaves "x".
+
+withoutString("Hello there", "llo") → "He there"
+withoutString("Hello there", "e") → "Hllo thr"
+withoutString("Hello there", "x") → "Hello there"
+*/
+
+public String withoutString(String base, String remove) {
+  String result="";
+  String b=base.toLowerCase();
+  String r=remove.toLowerCase();
+  for(int i=0;i<base.length();i++){
+    if(!b.substring(i).startsWith(r)){
+     result+=base.charAt(i);
+    }
+    else{
+      i+=remove.length()-1;
+    }
+  }
+  return result;
+}
